6c14b7b966c87d4440c4bd89cec730568ec20834
dattahexafold <dattatraya.ghadage@hexafoldtech.com>
2023-06-13 11:37:18 +0530

    application created

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <Navbar/>
+        <h1>default layout</h1>
+        <slot />
+    </div>
+</template>
+<script>
+import Navbar from '~/components/navbar.vue';
+
+export default {
+    components: { Navbar }
+}
+</script>
+<style>
+    
+</style>
